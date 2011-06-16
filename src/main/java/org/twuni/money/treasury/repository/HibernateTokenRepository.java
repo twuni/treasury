bb15b0821d36d275fcca4b8aee4110fd80ea62cf
Leo Accend <github.aeonic@spamgourmet.com>
2011-06-16 12:16:51 -0700

    Extending cash-api's TreasuryService and using Spring to configure it.

--- src/main/java/org/twuni/money/treasury/repository/HibernateTokenRepository.java
+++ src/main/java/org/twuni/money/treasury/repository/HibernateTokenRepository.java
@@ -1,6 +1,11 @@
 package org.twuni.money.treasury.repository;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.twuni.money.common.Repository;
 import org.twuni.money.common.Token;
@@ -25,6 +30,25 @@ public class HibernateTokenRepository implements Repository<String, Token> {
 	@Override
 	public void delete( Token dollar ) {
 		sessionFactory.getCurrentSession().delete( dollar );
+	}
+
+	@SuppressWarnings( "unchecked" )
+	@Override
+	public List<Token> list( int limit ) {
+
+		Session session = sessionFactory.getCurrentSession();
+		Criteria criteria = session.createCriteria( TokenEntity.class );
+
+		criteria.addOrder( Order.asc( "value" ) );
+		criteria.setMaxResults( limit );
+
+		return (List<Token>) criteria.list();
+
+	}
+
+	@Override
+	public List<Token> list() {
+		return list( Integer.MAX_VALUE );
 	}
 
 }
