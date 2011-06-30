77d476c98153a9714a225b207e3a0daeb8f4ec2f
Leo Accend <github.aeonic@spamgourmet.com>
2011-06-30 12:45:50 -0700

    Adding JavaScript dependencies for index.html. Explicitly mapping the spring servlet to /merge, /split, and /value to allow index.html and its dependencies to be served. Explicitly calling /treasury/* from index.html to ensure that the treasury's service URI starts with /treasury/.

---
+++ src/main/webapp/js/treasury.js
@@ -0,0 +1,56 @@
+$( function() {
+
+    $( "form[action='treasury/create'], form[action='treasury/merge']" ).submit( function( event ) {
+        
+        var form = $(this);
+
+        jQuery.post( $(this).attr( "action" ), $(this).serialize(), function( dollar ) {
+
+            $( ".response", form ).empty().append( $("<div>").text( dollar ) ).qrcode( {
+                typeNumber: 8,
+                correctLevel: 0,
+                text: dollar
+            } );
+
+        }, "text" );
+
+        event.preventDefault();
+        
+    } );
+
+    $( "form[action='treasury/value']" ).submit( function( event ) {
+        
+        var form = $(this);
+
+        jQuery.get( $(this).attr( "action" ), $(this).serialize(), function( value ) {
+            $( ".response", form ).empty().text( value );
+        }, "text" );
+        
+        event.preventDefault();
+        
+    } );
+
+    $( "form[action='treasury/split']" ).submit( function( event ) {
+        
+        var form = $(this);
+
+        jQuery.post( $(this).attr( "action" ), $(this).serialize(), function( dollars ) {
+
+            $( ".response", form ).empty();
+
+            for( var i = 0; i < dollars.length; i++ ) {
+                var dollar = JSON.stringify( dollars[i] );
+                $( ".response", form ).append( $("<div>").text( dollar ) ).qrcode( {
+                    typeNumber: 8,
+                    correctLevel: 0,
+                    text: dollar
+                } );
+            }
+
+        } );
+        
+        event.preventDefault();
+        
+    } );
+
+} );
