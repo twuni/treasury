ce7456dd6f1f9d25c3d89153c6d1a579d9cf6b8c
Leo Accend <github.aeonic@spamgourmet.com>
2011-06-30 15:26:27 -0700

    Allowing local addresses to use the Create API.

--- src/main/java/org/twuni/money/treasury/controller/TokenController.java
+++ src/main/java/org/twuni/money/treasury/controller/TokenController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.twuni.money.common.SimpleToken;
 import org.twuni.money.common.Token;
-import org.twuni.money.common.Treasury;
+import org.twuni.money.common.TreasuryService;
 
 import com.google.gson.Gson;
 
@@ -26,71 +26,80 @@ public class TokenController {
 	private final Logger log = LoggerFactory.getLogger( getClass() );
 
 	@Autowired
-	private Treasury treasury;
+	private TreasuryService treasury;
+	
+	@RequestMapping( method = RequestMethod.POST, value = "/create" )
+	public void create( @RequestParam final int value, HttpServletRequest request, HttpServletResponse response ) {
+		try {
+			if( !request.getRemoteAddr().equals( request.getLocalAddr() ) ) {
+				throw new IllegalAccessError( String.format( "%s != %s", request.getRemoteAddr(), request.getLocalAddr() ) );
+			}
+			Token token = treasury.create( value );
+			sendAsJson( token, request, response );
+		} catch( IllegalAccessError exception ) {
+			log.warn( String.format( "[%s] Illegal attempt to create token.", request.getRemoteAddr() ) );
+			response.setStatus( 401 );
+		} catch( Exception exception ) {
+			failGracefully( request, response, exception );
+		}
+	}
 
 	@RequestMapping( method = RequestMethod.POST, value = "/merge" )
 	public void merge( @RequestParam final String id1, @RequestParam final String secret1, @RequestParam final String id2, @RequestParam final String secret2, HttpServletRequest request, HttpServletResponse response ) {
-
-		Token a = new SimpleToken( id1, secret1 );
-		Token b = new SimpleToken( id2, secret2 );
-
 		try {
-
+			Token a = new SimpleToken( id1, secret1 );
+			Token b = new SimpleToken( id2, secret2 );
 			Token token = treasury.merge( a, b );
-
 			sendAsJson( token, request, response );
-
 		} catch( IllegalArgumentException exception ) {
 			log.info( String.format( "[%s] Error merging token: %s", request.getRemoteAddr(), exception.getMessage() ) );
 			response.setStatus( 400 );
+		} catch( Exception exception ) {
+			failGracefully( request, response, exception );
 		}
-
 	}
 
 	@RequestMapping( method = RequestMethod.POST, value = "/split" )
 	public void split( @RequestParam final String id, @RequestParam final String secret, @RequestParam int value, HttpServletRequest request, HttpServletResponse response ) {
-
-		Token token = new SimpleToken( id, secret );
-
 		try {
-
+			Token token = new SimpleToken( id, secret );
 			Set<Token> tokens = treasury.split( token, value );
-
 			sendAsJson( tokens, request, response );
-
 		} catch( IllegalArgumentException exception ) {
 			log.info( String.format( "[%s] Error splitting token: %s", request.getRemoteAddr(), exception.getMessage() ) );
 			response.setStatus( 400 );
+		} catch( Exception exception ) {
+			failGracefully( request, response, exception );
 		}
-
 	}
 
 	@RequestMapping( method = RequestMethod.GET, value = "/value" )
 	public void evaluate( @RequestParam String id, HttpServletRequest request, HttpServletResponse response ) {
-
-		Token token = new SimpleToken( id, null );
-
-		int value = treasury.getValue( token );
-
-		sendAsJson( Integer.valueOf( value ), request, response );
-
+		try {
+			Token token = new SimpleToken( id, null );
+			int value = treasury.getValue( token );
+			sendAsJson( Integer.valueOf( value ), request, response );
+		} catch( Exception exception ) {
+			failGracefully( request, response, exception );
+		}
 	}
 
 	private <T> void sendAsJson( T object, HttpServletRequest request, HttpServletResponse response ) {
-
 		try {
-
 			String json = new Gson().toJson( object );
 			response.setContentType( "application/json" );
 			Writer writer = response.getWriter();
 			writer.write( json );
 			writer.flush();
 			writer.close();
-
 		} catch( IOException exception ) {
 			log.warn( String.format( "[%s] Error sending response: %s", request.getRemoteAddr(), exception.getMessage() ) );
 		}
-
 	}
+
+	private void failGracefully( HttpServletRequest request, HttpServletResponse response, Exception exception ) {
+	    log.error( String.format( "[%s] %s", request.getRemoteAddr(), exception.getMessage() ) );
+	    response.setStatus( 500 );
+    }
 
 }
